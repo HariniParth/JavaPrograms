c22615e46ce70b6efabd3ca03c76a633fb0909fb
hariniparth <harini.parthasarathy@ge.com>
2017-11-26 21:41:54 -0800

    Repeated Subsequence

---
+++ SecondInterview/RepeatingSubSequence/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public int repeatSeq(String s){
+
+        int n = s.length();
+        int[][] count = new int[n+1][n+1];
+
+        for(int i=0;i<=n;i++){
+            Arrays.fill(count, 0);
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s.charAt(i) == s.charAt(j) && i != j)
+                    count[i][j] = count[i-1][j-1] + 1;
+                else
+                    count[i][j] = Math.max(count[i-1][j], count[i][j-1]);
+            }
+        }
+
+        return count[n][n] >= 2 ? 1 : 0;
+    }
+}
