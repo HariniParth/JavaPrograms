d9a0824a93b51486efbd55a2cf50892b75f0145a
hariniparth <harini.parthasarathy@ge.com>
2017-12-09 21:09:34 -0800

    Reverse stack

---
+++ SecondInterview/Stacks/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    Stack<Character> stack = new Stack<>();
+
+    public void reverse(){
+        if(stack.size() > 0){
+            char c = stack.peek();
+            stack.pop();
+            reverse();
+            insert(c);
+        }
+    }
+
+    public void insert(char c){
+        if(stack.size() == 0){
+            stack.push(c);
+        } else {
+            char x = stack.peek();
+            stack.pop();
+            insert(c);
+            stack.push(x);
+        }
+    }
+}
