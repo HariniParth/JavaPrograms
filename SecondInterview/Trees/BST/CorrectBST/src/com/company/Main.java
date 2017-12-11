a80b47141f6ecbb844131968791145954aae0aa9
hariniparth <harini.parthasarathy@ge.com>
2017-12-11 15:06:56 -0800

    Swap nodes in BST

---
+++ SecondInterview/Trees/BST/CorrectBST/src/com/company/Main.java
@@ -0,0 +1,55 @@
+package com.company;
+
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int d){
+        val = d;
+        left = null;
+        right = null;
+    }
+}
+
+public class Main {
+
+    TreeNode first, mid, last, prev;
+
+    public void correctBST(TreeNode node){
+
+        first = mid = last = prev = null;
+
+        correctBSTUtil(node);
+
+        if(first != null && last != null){
+            int temp = first.val;
+            first.val = last.val;
+            last.val = temp;
+        }
+        else if(first != null && mid != null){
+            int temp = first.val;
+            first.val = mid.val;
+            mid.val = temp;
+        }
+    }
+
+    public void correctBSTUtil(TreeNode node){
+        if(node != null){
+            correctBSTUtil(node.left);
+
+            if(prev != null && prev.val > node.val){
+                if(first == null){
+                    first = prev;
+                    mid = node;
+                } else {
+                    last = node;
+                }
+            } else {
+                prev = node;
+            }
+
+            correctBSTUtil(node.right);
+        }
+    }
+}
