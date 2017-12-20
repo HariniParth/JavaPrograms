84280de359714ec2f0346cefb3f2c5b8cf1ddb62
hariniparth <harini.parthasarathy@ge.com>
2017-12-20 12:34:58 -0800

    Substring with K Unique Characters

---
+++ SecondInterview/Strings/LongestSubstringKUnique/src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public int subStringKUnique(String s, int k){
+
+        if(s == null || s.length() == 0 || k == 0)
+            return 0;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        int n = s.length();
+        int i=0, j=0, ans=Integer.MIN_VALUE;
+
+        for(i=0;i<n;i++){
+            while(j < n){
+                char ch_j = s.charAt(j);
+                if(!map.containsKey(ch_j)){
+                    if(map.size() == k)
+                        break;
+                    else
+                        map.put(ch_j, 1);
+                } else {
+                    map.put(ch_j, map.get(ch_j)+1);
+                }
+                j++;
+            }
+
+            ans = Math.max(ans, j-i);
+            char ch_i = s.charAt(i);
+            if(map.containsKey(ch_i)){
+                if(map.get(ch_i) - 1 == 0)
+                    map.remove(ch_i);
+                else
+                    map.put(ch_i, map.get(ch_i) - 1);
+            }
+        }
+        
+        return ans;
+    }
+}
