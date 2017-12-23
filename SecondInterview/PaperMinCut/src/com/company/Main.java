6bd383ea204672743049de58574e726719bfb12b
hariniparth <harini.parthasarathy@ge.com>
2017-12-23 08:50:54 -0800

    Paper Min Cut

---
+++ SecondInterview/PaperMinCut/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public int minCut(int m, int n){
+
+        if(n > m){
+            m = m + n;
+            n = m - n;
+            m = m - n;
+        }
+
+        long result = 0, rem = 0;
+
+        while(n > 0){
+            result += m/n;
+            rem = m % n;
+            m = n;
+            n = (int) rem;
+        }
+        
+        return (int) result;
+    }
+}
