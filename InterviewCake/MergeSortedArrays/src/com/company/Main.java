2d2bc959cc61cf37b57a8d1303abc1a9b30f066b
hariniparth <harini.parthasarathy@ge.com>
2018-01-06 08:01:21 -0800

    Merge Sorted Arrays

--- InterviewCake/MergeSortedArrays/src/com/company/Main.java
+++ InterviewCake/MergeSortedArrays/src/com/company/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public int[] mergeSortedArrays(int[] arr1, int[] arr2){
 
-        int[] arr3 = new int[];
+        int n = arr1.length + arr2.length;
+        int[] arr3 = new int[n];
         int i = 0, j = 0, k = 0;
 
         while(i < arr1.length && j < arr2.length){
