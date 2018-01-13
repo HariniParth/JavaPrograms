af28b35744c92e272abdcb84c9b2b80e907209df
hariniparth <harini.parthasarathy@ge.com>
2018-01-12 23:14:19 -0800

    Selection Sort

---
+++ Sort and Search/Selection Sort/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public void sort(int[] arr){
+        int n = arr.length;
+        for(int i=0;i<n-1;i++){
+            int minIndex = i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j] < arr[minIndex])
+                    minIndex = j;
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
