f10ad3a2e0184f1f3768ed4b9775899e945eaf6b
HariniParth <parth.harini@gmail.com>
2018-07-04 22:06:52 -0400

    MS trees part 1

---
+++ Third Interview/Microsoft/ConnectRootNodes/src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode nextRight;
+
+    TreeNode(int d){
+        val = d;
+        left = null;
+        right = null;
+        nextRight = null;
+    }
+}
+
+public class Main {
+
+    public void connect(TreeNode node){
+
+        node.nextRight = null;
+
+        connectUtil(node);
+    }
+
+    public void connectUtil(TreeNode node){
+
+        if(node == null)
+            return;
+
+        if(node.left != null)
+            node.left.nextRight = node.right;
+
+        if(node.right != null)
+            node.right.nextRight = (node.nextRight != null)? node.nextRight.left : null;
+
+        connectUtil(node.left);
+        connectUtil(node.right);
+    }
+}
