8b82f0f2c222f53e4d9fdccfb14a5a7fc2d9fd79
HariniParth <parth.harini@gmail.com>
2018-07-14 20:03:48 -0400

    One tree is a subtree of another

---
+++ Third Interview/Microsoft/Trees/SubTree/src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int d){
+        val = d;
+        left = null;
+        right = null;
+    }
+}
+
+public class Main {
+
+    public boolean isSubTree(TreeNode node1, TreeNode node2){
+
+        if(node2 == null)
+            return true;
+
+        if(node1 == null)
+            return false;
+
+        if(isIdentical(node1, node2))
+            return true;
+
+        return (isSubTree(node1.left, node2) || isSubTree(node1.right, node2));
+    }
+
+    public boolean isIdentical(TreeNode node1, TreeNode node2){
+
+        if(node1 == null && node2 == null)
+            return true;
+
+        if(node1 == null || node2 == null)
+            return false;
+
+        return isIdentical(node1.left, node2.left) && isIdentical(node1.right, node2.right);
+    }
+}
