6cfecbeb9b0ac99ba2e0d94a317b7be6a8b7b0ce
HariniParth <parth.harini@gmail.com>
2018-10-09 21:41:22 -0400

    Algo part 2

---
+++ Third Interview/Microsoft/Algorithms/MergeTwoSorted/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    private void mergeTwoSorted(int[] arr1, int n1, int[] arr2, int n2, int[] arr3){
+
+        int i=0, j=0, k=0;
+
+        while(i < n1 && j < n2){
+            if(arr1[i] < arr2[j]){
+                arr3[k++] = arr1[i++];
+            } else {
+                arr3[k++] = arr2[j++];
+            }
+        }
+
+        while(i < n1)
+            arr3[k++] = arr1[i++];
+
+        while(j < n2)
+            arr3[k++] = arr2[i++];
+    }
+}
