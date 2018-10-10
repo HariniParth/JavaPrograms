7aa002dc68f7d42c8b2fd87e17199facb85ea413
HariniParth <parth.harini@gmail.com>
2018-10-09 22:24:30 -0400

    MS Strings part 3

---
+++ Third Interview/Microsoft/Strings/LongestValidParanthesis/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    private int longestParan(String s){
+
+        int max = 0;
+        for(int i=0;i<s.length();i++){
+            for(int j=i+1;j<s.length();j++){
+                if(isValid(s.substring(i,j)))
+                    max = Math.max(max, j-i);
+            }
+        }
+        return max;
+    }
+
+    private boolean isValid(String s){
+
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == '(')
+                stack.push(s.charAt(i));
+            else if(!stack.isEmpty() && stack.peek() == '(')
+                stack.pop();
+            else
+                return false;
+        }
+        return stack.isEmpty();
+    }
+}
