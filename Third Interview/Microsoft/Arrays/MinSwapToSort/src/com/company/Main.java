96c868ef7328fd597d65acf5c3172f436814c118
HariniParth <parth.harini@gmail.com>
2018-11-26 19:40:59 -0500

    MS Arrays part 2

---
+++ Third Interview/Microsoft/Arrays/MinSwapToSort/src/com/company/Main.java
@@ -0,0 +1,50 @@
+package com.company;
+
+import javafx.util.Pair;
+
+import java.util.*;
+
+// https://www.geeksforgeeks.org/minimum-number-swaps-required-sort-array/
+
+public class Main {
+
+    private int minSwapToSort(int[] arr){
+
+        ArrayList<Pair<Integer, Integer>> arrPos = new ArrayList<>();
+        for(int i=0;i<arr.length;i++)
+            arrPos.add(new Pair<>(arr[i], i));
+
+        arrPos.sort(new Comparator<Pair<Integer, Integer>>(){
+            public int compare(Pair<Integer, Integer> p1, Pair<Integer, Integer> p2){
+                if(p1.getKey() > p2.getKey())
+                    return -1;
+                else if(p1.getKey().equals(p2.getKey()))
+                    return 0;
+                else
+                    return 1;
+            }
+        });
+
+        boolean[] visited = new boolean[arr.length];
+        Arrays.fill(visited, false);
+        int ans = 0;
+
+        for(int i=0;i<arr.length;i++){
+
+            if(visited[i] || arrPos.get(i).getValue() == i)
+                continue;
+
+            int cycle = 0;
+            int j = i;
+            if(!visited[j]){
+                visited[j] = true;
+                j = arrPos.get(j).getValue();
+                cycle++;
+            }
+            if(cycle > 0){
+                ans += cycle-1;
+            }
+        }
+        return ans;
+    }
+}
