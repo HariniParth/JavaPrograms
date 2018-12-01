5092473148a12e7853de9adc7602f7b7d65c5252
HariniParth <parth.harini@gmail.com>
2018-12-01 18:26:03 -0500

    Max level sum

--- SecondInterview/Queue/MaxLevelSum/src/com/company/Main.java
+++ SecondInterview/Queue/MaxLevelSum/src/com/company/Main.java
@@ -2,6 +2,8 @@ package com.company;
 
 import java.util.*;
 
+// https://www.geeksforgeeks.org/find-level-maximum-sum-binary-tree/
+
 class TreeNode{
     int val;
     TreeNode left, right;
@@ -14,15 +16,19 @@ class TreeNode{
 
 public class Main {
 
-    public int maxLevelSum(TreeNode node){
+    private int maxLevelSum(TreeNode node){
+
+        if(node == null)
+            return 0;
+
         Queue<TreeNode> queue = new LinkedList<>();
+        int result = 0;
         queue.add(node);
 
-        int result = node.val;
         while(!queue.isEmpty()){
-            int count = queue.size();
             int sum = 0;
-            while(count-- >= 0){
+            int count = 0;
+            while(count-- > 0){
                 TreeNode n = queue.peek();
                 queue.remove();
 
