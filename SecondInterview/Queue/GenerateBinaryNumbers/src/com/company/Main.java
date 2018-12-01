8b44a48d796133d5fabc3ff98b32824c5a6c7f2c
HariniParth <parth.harini@gmail.com>
2018-12-01 17:21:06 -0500

    Generate Binary Numbers

--- SecondInterview/Queue/GenerateBinaryNumbers/src/com/company/Main.java
+++ SecondInterview/Queue/GenerateBinaryNumbers/src/com/company/Main.java
@@ -4,17 +4,18 @@ import java.util.*;
 
 public class Main {
 
-    public void generateBinary(int n){
+    private void generateBinaryNumbers(int n){
+
         Queue<String> queue = new LinkedList<>();
         queue.add("1");
 
         while(n-- > 0){
             String s1 = queue.peek();
             queue.remove();
-            System.out.println(s1);
+            System.out.print(s1+"\n");
 
             String s2 = s1;
-            queue.add(s1+"0");
+            queue.add(s2+"0");
             queue.add(s2+"1");
         }
     }
