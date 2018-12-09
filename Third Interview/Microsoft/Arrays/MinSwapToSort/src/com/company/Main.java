f4c5339bb2a5efc112b1fc996ccfb97e46fd6ce9
HariniParth <parth.harini@gmail.com>
2018-12-09 17:54:55 -0500

    MS Arrays part 2

--- Third Interview/Microsoft/Arrays/MinSwapToSort/src/com/company/Main.java
+++ Third Interview/Microsoft/Arrays/MinSwapToSort/src/com/company/Main.java
@@ -8,43 +8,45 @@ import java.util.*;
 
 public class Main {
 
-    private int minSwapToSort(int[] arr){
+    private int minSwapsToSort(int[] arr){
 
         ArrayList<Pair<Integer, Integer>> arrPos = new ArrayList<>();
-        for(int i=0;i<arr.length;i++)
+        for(int i=0;i<arr.length;i++){
             arrPos.add(new Pair<>(arr[i], i));
+        }
 
         arrPos.sort(new Comparator<Pair<Integer, Integer>>(){
             public int compare(Pair<Integer, Integer> p1, Pair<Integer, Integer> p2){
-                if(p1.getKey() > p2.getKey())
+                if(p1.getKey() > p2.getKey()){
                     return -1;
-                else if(p1.getKey().equals(p2.getKey()))
+                }
+                else if(p1.getKey().equals(p2.getKey())){
                     return 0;
-                else
+                }
+                else {
                     return 1;
+                }
             }
         });
 
         boolean[] visited = new boolean[arr.length];
         Arrays.fill(visited, false);
-        int ans = 0;
+        int result = 0;
 
         for(int i=0;i<arr.length;i++){
-
             if(visited[i] || arrPos.get(i).getValue() == i)
                 continue;
 
-            int cycle = 0;
             int j = i;
-            if(!visited[j]){
+            int carry = 0;
+            while(!visited[j]){
                 visited[j] = true;
                 j = arrPos.get(j).getValue();
-                cycle++;
+                carry++;
             }
-            if(cycle > 0){
-                ans += cycle-1;
-            }
+            if(carry > 0)
+                result += carry - 1;
         }
-        return ans;
+        return result;
     }
 }
